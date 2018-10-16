2560bd5dce8433bc3086d1d42cc7679acd1eb434
Louis Coilliot <louis.coilliot@gmail.com>
2018-10-16 09:54:31 +0200

    Add locations, colocations, orders, clones, rsc_defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,9 +14,19 @@ class cswrap(
 
     cs_shadow { 'puppet': }
 
-    $primitives.each |$k, $v| { cs_primitive { $k: * => $v } }
+    $primitives.each   |$k, $v| { cs_primitive    { $k: * => $v } }
 
-    $properties.each |$k, $v| { cs_property  { $k: * => $v } }
+    $locations.each    |$k, $v| { cs_location     { $k: * => $v } }
+
+    $colocations.each  |$k, $v| { cs_colocation   { $k: * => $v } }
+
+    $orders.each       |$k, $v| { cs_order        { $k: * => $v } }
+
+    $clones.each       |$k, $v| { cs_clone        { $k: * => $v } }
+
+    $properties.each   |$k, $v| { cs_property     { $k: * => $v } }
+
+    $rsc_defaults.each |$k, $v| { cs_rsc_defaults { $k: * => $v } }
 
     cs_commit { 'puppet': }
 
